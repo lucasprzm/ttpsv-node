3675e5defcb3207aae59606b923a026cd2993921
Lucas Prazeres de Matos <lucasprzm@gmail.com>
2024-01-10 22:40:30 -0300

    rules for new carDriver

--- src/controllers/carsdrivers/CreateCarDriverController.ts
+++ src/controllers/carsdrivers/CreateCarDriverController.ts
@@ -5,7 +5,24 @@ export class CreateCarDriverController {
   async handle(request: Request, response: Response) {
     const { reason, carId, driverId } = request.body;
 
-    // #TODO fazer regras para verificar carro sendo usado e motorista já dirigindo outro carro
+    const carBeingUsed = await checkCarBeingUsed(carId);
+
+    if (carBeingUsed) {
+      return response.status(400).json({
+        message:
+          "Automóvel já está sendo usado, finalize o vínculo anterior para completar a nova vinculação.",
+      });
+    }
+
+    const driverBeingUsed = await checkDriverBeingUsed(driverId);
+
+    if (driverBeingUsed) {
+      return response.status(400).json({
+        message:
+          "Motorista utilizando outro veículo, finalize o vínculo anterior para completar a nova vinculação.",
+      });
+    }
+
     const carDriver = await prismaClient.carDriver.create({
       data: {
         reason,
@@ -16,5 +33,47 @@ export class CreateCarDriverController {
 
     console.log(`Driver linked to car succesfully!`);
     return response.status(201).json({ message: "Motorista vinculado ao carro com sucesso!" });
+
+    async function checkCarBeingUsed(carId: number): Promise<boolean> {
+      const carBeingUsed =
+        (await prismaClient.carDriver.count({
+          where: {
+            AND: {
+              carId: {
+                equals: carId,
+              },
+              finishedUsing: {
+                equals: null,
+              },
+              removedAt: {
+                equals: null,
+              },
+            },
+          },
+        })) > 0;
+
+      return carBeingUsed;
+    }
+
+    async function checkDriverBeingUsed(driverId: number): Promise<boolean> {
+      const driverBeingUsed =
+        (await prismaClient.carDriver.count({
+          where: {
+            AND: {
+              driverId: {
+                equals: driverId,
+              },
+              finishedUsing: {
+                equals: null,
+              },
+              removedAt: {
+                equals: null,
+              },
+            },
+          },
+        })) > 0;
+
+      return driverBeingUsed;
+    }
   }
 }
